9dede189bb4c780912e97875b1f121fb3344c4eb
Marine Fassin <128480249+MarineFsn@users.noreply.github.com>
2023-04-06 16:38:08 +0200

    Exo 2.6

---
+++ Javascript/Javascript.js
@@ -0,0 +1,35 @@
+let daynumber = parseInt(prompt("Enter a number between 1 to 7"));
+
+switch (daynumber) {
+    case 1:
+        console.log ("Monday");
+        break;
+
+    case 2:
+        console.log ("Tuesday");
+            break;
+    case 3:
+        console.log ("wednesday");
+             break;   
+
+    case 4:
+        console.log ("Thursday");
+             break;
+
+    case 5:
+        console.log ("friday");   
+             break;
+    case 6:
+        console.log ("saturday");
+             break;
+    case 7:
+        console.log ("sunday");
+             break;
+
+default : console.log ("invalid input. please enter a number between 1 and 7")
+
+
+    
+
+
+        }
