3f095334ea7e7b00ce4e6d087abd1c4994c76d2a
Marine Fassin <128480249+MarineFsn@users.noreply.github.com>
2023-04-06 14:26:29 +0200

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,11 +1,3 @@
-let min = parseInt(prompt("Enter the minimum number: "));
-let max = parseInt(prompt("Enter the maximum number: "));
-let current = parseInt(prompt("Enter the current number:"));
-
-if (current >=min && current <=max) {
-    console.log ("you win!")
-}
-
-else if (min > max) {
-    console.log ("ERROR")
+for (let i =0; i <= 100; i+=2) {
+    console.log (i);
 }
